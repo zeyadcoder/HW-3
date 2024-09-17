491bfecd593f93c6e7325f99070e930b72795c45
zeyadcoder <Zeyad.bawany@gmail.com>
2024-09-17 22:11:37 +0500

    work5

---
+++ homework5.py
@@ -0,0 +1,11 @@
+money = input("insufficent funds")
+bill = int(input("enter bill amount: "))
+def my_func():
+    return(money)
+if bill > 4.50:
+    print("U have paid enough")
+elif bill < 4.50:
+    print(my_func)
+
+
+
